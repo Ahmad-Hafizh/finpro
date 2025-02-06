29e6a67e427d308ce35ee7267518c13a3bed1efc
Kusuma Ningrum <155100839+kusumaningrum-code@users.noreply.github.com>
2025-02-07 00:16:59 +0700

    edit-schema-f3 (#2)

--- packages/database/generated/client/edge.js
+++ packages/database/generated/client/edge.js
@@ -339,7 +339,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/macbook/Documents/finpro/finpro/packages/database/generated/client",
+      "value": "E:\\CODE\\finpro-alv\\finpro\\packages\\database\\generated\\client",
       "fromEnvVar": null
     },
     "config": {
@@ -348,12 +348,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "darwin-arm64",
+        "value": "windows",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/macbook/Documents/finpro/finpro/packages/database/prisma/schema.prisma",
+    "sourceFilePath": "E:\\CODE\\finpro-alv\\finpro\\packages\\database\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
