7fcbcf2a375da29128d9ca4877b0b2be6b2bdd09
kusumaningrum-code <kusumaningrum416@gmail.com>
2025-02-27 00:39:26 +0700

    order management backend dan front end

---
+++ apps/backend/src/routers/adminOrder.router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { AdminOrderController } from "../controllers/adminOrder.controller";
+
+export class AdminOrderRouter {
+  private route: Router;
+  private adminOrderController: AdminOrderController;
+
+  constructor() {
+    this.route = Router();
+    this.adminOrderController = new AdminOrderController();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.route.get("/", this.adminOrderController.getAllOrders);
+    this.route.post(
+      "/:order_id/confirm-payment",
+      this.adminOrderController.confirmPayment
+    );
+    this.route.post("/:order_id/send", this.adminOrderController.sendUserOrder);
+    this.route.post(
+      "/:order_id/cancel",
+      this.adminOrderController.cancelUserOrder
+    );
+  }
+
+  public getRouter(): Router {
+    return this.route;
+  }
+}
