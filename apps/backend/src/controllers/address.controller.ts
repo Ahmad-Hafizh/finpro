a34ac9060c027c710779efc6009062381a80ac9a
Ahmad Hafizh <140679217+Ahmad-Hafizh@users.noreply.github.com>
2025-03-02 18:07:20 +0300

    User update (#46)
    * prep deploy
    
    * pull 26-feb
    
    * merge deploy
    
    * fix auth
    
    * address and auth fix
    
    * address and auth fix

--- apps/backend/src/controllers/address.controller.ts
+++ apps/backend/src/controllers/address.controller.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
-import prisma from "../prisma";
-import ResponseHandler from "../utils/responseHandler";
+import { Request, Response } from 'express';
+import prisma from '../prisma';
+import ResponseHandler from '../utils/responseHandler';
 
 export class AddressController {
   async getAddresses(req: Request, res: Response): Promise<any> {
@@ -14,21 +14,15 @@ export class AddressController {
           profile: true,
         },
       });
-      if (!user?.profile)
-        return ResponseHandler.error(res, 404, "user not found");
+      if (!user?.profile) return ResponseHandler.error(res, 404, 'user not found');
 
       const addresses = await prisma.address.findMany({
         where: { profile_id: user.profile.profile_id, deleted_at: null },
       });
 
-      return ResponseHandler.success(
-        res,
-        200,
-        "get address success",
-        addresses
-      );
+      return ResponseHandler.success(res, 200, 'get address success', addresses);
     } catch (error) {
-      return ResponseHandler.error(res, 500, "internal server error", error);
+      return ResponseHandler.error(res, 500, 'internal server error', error);
     }
   }
   // async getAddresses(req: Request, res: Response): Promise<any> {
@@ -62,15 +56,26 @@ export class AddressController {
         },
       });
 
+      if (!user?.profile?.profile_id) {
+        return ResponseHandler.error(res, 400, 'user not found');
+      }
+
       await prisma.address.create({
         data: {
-          ...req.body,
-          profile_id: user?.profile?.profile_id,
+          profile_id: user.profile.profile_id,
+          address_name: req.body.address_name,
+          address_contact: req.body.address_contact,
+          country: req.body.country,
+          city: req.body.city,
+          street: req.body.street,
+          post_code: req.body.post_code,
+          lat: req.body.lat,
+          lng: req.body.lng,
         },
       });
-      return ResponseHandler.success(res, 201, "add address success");
+      return ResponseHandler.success(res, 201, 'add address success');
     } catch (error) {
-      return ResponseHandler.error(res, 500, "internal server error", error);
+      return ResponseHandler.error(res, 500, 'internal server error', error);
     }
   }
   async updateDeliveryAddress(req: Request, res: Response): Promise<any> {
@@ -96,14 +101,14 @@ export class AddressController {
           ...req.body,
         },
       });
-      return ResponseHandler.success(res, 201, "update address success");
+      return ResponseHandler.success(res, 201, 'update address success');
     } catch (error) {
-      return ResponseHandler.error(res, 500, "internal server error", error);
+      return ResponseHandler.error(res, 500, 'internal server error', error);
     }
   }
   async deleteAddress(req: Request, res: Response): Promise<any> {
     try {
-      const { email, address_id } = req.body;
+      const { email, address_id } = req.params;
 
       const user = await prisma.user.findUnique({
         where: {
@@ -115,19 +120,19 @@ export class AddressController {
       });
 
       if (!user) {
-        return ResponseHandler.error(res, 404, "User not found");
+        return ResponseHandler.error(res, 404, 'User not found');
       }
 
       await prisma.address.update({
-        where: { address_id },
+        where: { address_id: parseInt(address_id) },
         data: {
           deleted_at: new Date().toISOString(),
         },
       });
 
-      return ResponseHandler.success(res, 200, "Delete Address Success");
+      return ResponseHandler.success(res, 200, 'Delete Address Success');
     } catch (error) {
-      return ResponseHandler.error(res, 500, "internal server error", error);
+      return ResponseHandler.error(res, 500, 'internal server error', error);
     }
   }
 }
