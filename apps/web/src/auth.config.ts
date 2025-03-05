c7aca4d3b2c3437cb3aa5bf024793fddef37dc19
langlangsatrio <langlangsatrio@gmail.com>
2025-03-05 15:16:11 +0700

    fix

--- apps/web/src/auth.config.ts
+++ apps/web/src/auth.config.ts
@@ -11,8 +11,8 @@ import { callAPI } from "@/config/axios";
 export default {
   providers: [
     Google({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Credentials({
       async authorize(credentials) {
